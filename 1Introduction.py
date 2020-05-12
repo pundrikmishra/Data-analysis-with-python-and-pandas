4a39406d66660304313f10884b2473e46407568f
amit <pundrikmishra1234@gmail.com>
2020-05-12 17:03:28 +0530

    12-05-2020

--- 1Introduction.py
+++ 1Introduction.py
@@ -3,8 +3,6 @@ import datetime
 import pandas_datareader as web
 
 from matplotlib import pyplot as plt
-# from matplotlib import style
-
 
 start = datetime.datetime(2020, 5, 1)
 end = datetime.datetime.now()
--- 4BuildingDataset.py
+++ 4BuildingDataset.py
@@ -6,7 +6,13 @@ import pandas as pd
 # print(df.head())
 
 
-state_name = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states_by_population')
-#
-print(state_name[0])
-# state_name.to_html('us_states.html')
+# state_name = pd.read_html('https://en.wikipedia.org/wiki/States_and_union_territories_of_India')
+# df= state_name[3]
+
+# for i in state_name[3][0][1:]:
+#     print(i)
+fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
+print(fiddy_states)
+print(fiddy_states[0])
+for abbv in fiddy_states[0][0][1:]:
+    print(abbv)
